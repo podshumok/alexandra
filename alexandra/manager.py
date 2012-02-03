f2971dc35453e622c50cafb740bcd5d90498cce4
Konstantin Podshumok <kpp.live@gmail.com>
2012-02-04 00:57:43 +0400

    make manager a little lazy

--- alexandra/manager.py
+++ alexandra/manager.py
@@ -1,6 +1,5 @@
-import pycassa
+#import pycassa
 import alexandra
-import inspect
 from django.conf import settings
 from django.db.models.manager import ManagerDescriptor
 
@@ -9,6 +8,7 @@ from alexandra import pools
 class MetaManager(type):
     def __init__(cls, name, bases, attrs):
         if settings.DEBUG:
+            import inspect
             from alexandra.logger import logged_func
 
             for base in bases:
@@ -21,6 +21,15 @@ class MetaManager(type):
                 if not attr.startswith('_') and inspect.isfunction(value):
                     setattr(cls, attr, logged_func(value))
 
+class AlexandraManagerDescriptor(ManagerDescriptor):
+    def __init__(self, manager, *args, **kwargs):
+        super(AlexandraManagerDescriptor, self).__init__(manager, *args, **kwargs)
+        self._manager = manager
+
+    def __get__(self, instance, *args, **kwargs):
+        if not self._manager._ready: self._manager._init()
+        return super(AlexandraManagerDescriptor, self).__get__(instance, *args, **kwargs)
+
 class Manager(alexandra.CF):
     
     __metaclass__ = MetaManager
@@ -30,17 +39,25 @@ class Manager(alexandra.CF):
     
     def contribute_to_class(self, model, name,):
         self.model = model
-        setattr(model, name, ManagerDescriptor(self))
+        setattr(model, name, AlexandraManagerDescriptor(self))
         meta = model._meta
         self.pool_name = meta.pool_name if meta.pool_name else 'default'
+        self._ready = False
+
+    def _init(self):
+        model = self.model
+        meta = model._meta
         pool = pools[self.pool_name]
         if pool is None:
             params = settings.CASSANDRA[self.pool_name]
             adv = params.get('PARAMS', {})
             pools[self.pool_name] = alexandra.Pool(params['KEYSPACE'], params['CLUSTER'], **adv)
             pool = pools[self.pool_name]
-        super(Manager, self).__init__(pool, meta.object_name, dict_class=model, super=meta.super_cf)
+        super(Manager, self).__init__(pools[self.pool_name],
+            meta.object_name,
+            dict_class=model, super=meta.super_cf)
         meta.apply_names(self)
+        self._ready = True
 
     def get(self, key, **kwargs):
         if 'column_count' not in kwargs:
@@ -73,4 +90,3 @@ class Manager(alexandra.CF):
         
     def _remove(self, key, column=None, write_consistency_level = None):
         return super(Manager, self).remove(key, column=column, write_consistency_level=write_consistency_level)
-    
